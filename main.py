27252a781df4cd80477b7ab29eb598cd10454cba
wyxogo <wyxogo@163.com>
2022-08-28 21:30:38 +0800

    add logging

--- main.py
+++ main.py
@@ -1,34 +1,82 @@
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
-import sys
+
+import os
+import time
 
 from vgg_model import build_model
 from datasets import get_dataset, get_dataloader
+from utils import get_logger, AverageMeter
 
-def train(dataloader: DataLoader, model: nn.Module, criterion, optimizer:torch.optim, epochs: int, device):
+
+def train(dataloader: DataLoader, 
+            model: nn.Module, 
+            criterion, 
+            optimizer:torch.optim, 
+            epoch,
+            total_epochs,
+            total_batch,
+            device,
+            debug_steps:int=20,
+            logger=None):
+    """Training for one epoch
+    Args:
+        dataloader: paddle.io.DataLoader, dataloader instance
+        model: nn.Layer, a ViT model
+        criterion: nn.criterion
+        epoch: int, current epoch
+        total_epochs: int, total num of epochs
+        total_batch: int, total num of batches for one epoch
+        debug_steps: int, num of iters to log info, default: 100
+        accum_iter: int, num of iters for accumulating gradients, default: 1
+        mixup_fn: Mixup, mixup instance, default: None
+        amp: bool, if True, use mix precision training, default: False
+        logger: logger for logging, default: None
+    Returns:
+        train_loss_meter.avg: float, average loss on current process/gpu
+        train_acc_meter.avg: float, average top1 accuracy on current process/gpu
+        train_time: float, training time
+    """
     model.train()
     model.to(device=device)
 
-    for e in range(epochs):
-        i = 0
-        total_loss = 0
-        for batch_id, data in enumerate(dataloader):
-            
-            samples = data[0].to(device, non_blocking=True)
-            labels = data[1].to(device, non_blocking=True)
-            optimizer.zero_grad()
-            outputs = model(samples)
-            # pre = outputs.argmax(dim=1)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            loss_value = loss.item()
-            optimizer.step()
-            total_loss += loss_value
-            i+=1
-            if i%20 == 0:
-                print(f'Epoch: {e+1:03d}, Iter: {batch_id+1:03d}, loss: {loss_value:.4f}')
-        print(f'Epoch: {e+1:03d}, Average Epoch Loss: {total_loss/i:.4f}')
+    train_loss_meter = AverageMeter()
+    train_acc_meter = AverageMeter()
+
+    time_start = time.time()
+
+    for batch_id, data in enumerate(dataloader):
+        
+        samples = data[0].to(device, non_blocking=True)
+        labels = data[1].to(device, non_blocking=True)
+        label_orig = labels.clone()
+
+        optimizer.zero_grad()
+        
+        outputs = model(samples)
+        
+        loss = criterion(outputs, labels)
+        
+        loss.backward()
+        optimizer.step()
+
+        pred = torch.argmax(outputs, dim=1)
+        acc = torch.eq(pred, label_orig).sum().float()
+
+        batch_size = samples.shape[0]
+        train_loss_meter.update(loss.item(), batch_size)
+        train_acc_meter.update(acc.item(), batch_size)
+
+        if logger and batch_id % debug_steps == 0:
+            logger.info(
+                f"Epoch[{epoch:03d}/{total_epochs:03d}], " +
+                f"Step[{batch_id:04d}/{total_batch:04d}], " +
+                f"Avg Loss: {train_loss_meter.avg:.4f}, " +
+                f"Avg Acc: {train_acc_meter.avg:.4f}")
+
+    train_time = time.time() - time_start
+    return train_loss_meter.avg, train_acc_meter.avg, train_time
 
 
 def main(mode):
@@ -42,21 +90,31 @@ def main(mode):
 
     batch_size = 32
     epochs = 200
+    learning_rate = 1e-4
+
+    logger = get_logger(filename=os.path.join('./outputs/', 'log.txt'))
+
 
     dataset = get_dataset(mean, std, mini_label_names, mode, data_path=data_path)
     dataloader = get_dataloader(dataset, batch_size)
     vgg19_model = build_model()
 
     criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(vgg19_model.parameters())
+    optimizer = torch.optim.Adam(vgg19_model.parameters(), lr=learning_rate)
 
     if mode == 'train':
-        train(dataloader=dataloader,
-              model=vgg19_model,
-              criterion=criterion,
-              optimizer=optimizer,
-              epochs = epochs,
-              device=device)
+        for epoch in range(epochs):
+            logger.info(f"Now training epoch {epoch+1}. LR={optimizer.state_dict()['param_groups'][0]['lr']:.6f}")
+            train_loss, train_acc, train_time = train(dataloader=dataloader,
+                                                        model=vgg19_model,
+                                                        criterion=criterion,
+                                                        optimizer=optimizer,
+                                                        epoch=epoch,
+                                                        total_epochs =epochs, 
+                                                        total_batch=len(dataloader),
+                                                        device=device,
+                                                        debug_steps=20,
+                                                        logger=logger)
 
 
 if __name__ == "__main__":
---
+++ utils.py
@@ -0,0 +1,41 @@
+import logging
+import os
+import sys
+
+class AverageMeter():
+    """ Meter for monitoring losses"""
+    def __init__(self):
+        self.avg = 0
+        self.sum = 0
+        self.cnt = 0
+        self.reset()
+
+    def reset(self):
+        """reset all values to zeros"""
+        self.avg = 0
+        self.sum = 0
+        self.cnt = 0
+
+    def update(self, val, n=1):
+        """update avg by val and n, where val is the avg of n values"""
+        self.sum += val * n
+        self.cnt += n
+        self.avg = self.sum / self.cnt
+
+def get_logger(filename, logger_name=None):
+    """set logging file and format
+    Args:
+        filename: str, full path of the logger file to write
+        logger_name: str, the logger name, e.g., 'master_logger', 'local_logger'
+    Return:
+        logger: python logger
+    """
+    log_format = "%(asctime)s %(message)s"
+    logging.basicConfig(stream=sys.stdout, level=logging.INFO,
+                        format=log_format, datefmt="%m%d %I:%M:%S %p")
+    # different name is needed when creating multiple logger in one process
+    logger = logging.getLogger(logger_name)
+    fh = logging.FileHandler(os.path.join(filename))
+    fh.setFormatter(logging.Formatter(log_format))
+    logger.addHandler(fh)
+    return logger
